5b110bc410467ba444e35d453cc10dbbc37fd964
Zaen1993 <nooralmostafa47@gmail.com>
2026-03-18 00:44:13 +0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,9 +1,5 @@
 import time
 
-def run():
-    while True:
-        print("Service is alive")
-        time.sleep(60)
-
-if __name__ == '__main__':
-    run()
+while True:
+    print("Service monitoring...")
+    time.sleep(30)
