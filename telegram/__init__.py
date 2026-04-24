b547e05b18b82df28bb993f791925b25d1fdcb04
Zaen1993 <nooralmostafa47@gmail.com>
2026-04-24 04:06:38 +0300

    Delete telegram directory

--- telegram/__init__.py
+++
@@ -1,6 +0,0 @@
-# telegram/__init__.py
-# هذا الملف يُعرِّف مجلد telegram كحزمة Python قابلة للاستيراد.
-# يحتوي على وحدات الاتصال بـ Telegram API وإدارة البوت.
-
-__version__ = "2.0"
-__all__ = ["webhook", "telegram_ui", "commands"]
